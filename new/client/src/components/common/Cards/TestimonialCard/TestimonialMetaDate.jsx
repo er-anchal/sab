d4320423acf8775270c9d291f1f8794f3064146f
Anchal Vaishya Banwarilal <er.anchalvaishya@gmail.com>
2026-02-07 07:50:15 +0530

    "added"

--- new/client/src/components/common/Cards/TestimonialCard/TestimonialMetaDate.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TestimonialMetaDate = ({ date }) => {
-  return (
-    <p className="text-[rgb(83,82,82)] text-xs min-[500px]:text-sm mb-2">{date}</p>
-  );
-};
-
-export default TestimonialMetaDate;
